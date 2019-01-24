5d983dda58494e9dbcb0fe49e1be18f8f565f33f
philshen <amyfanpti@gmail.com>
2019-01-24 14:30:07 +0800

    Update user privilege in README.md and add sequential_FromTwseToMongo.py

--- README.md
+++ README.md
@@ -2,7 +2,105 @@ MongoDB_TSEOTC_Crawler
 ==============================
 TW TSE&amp;OTC OHLC Price Crawler to store in MongoDB
 
+MongoDB Setup:
 ==============================
+* Step 1
+MongoDB command shell
+``` 
+$ mongo
+``` 
+![alt tag](https://i.imgur.com/hCcWrqZ.jpg)
+
+* Step 2
+Disable MongoDB Auth
+``` 
+$ sudo vim /etc/mongod.conf
+``` 
+
+In this file remark the following lines:
+``` 
+#security:
+#    authorization: 'enabled'
+``` 
+
+Now save and exit the config file and restart mongodb server.
+``` 
+$ sudo service mongod restart
+``` 
+
+* Step 3
+Swtich to DB and Create Read/Write Privilege for User
+``` 
+> use TWStockDB
+``` 
+
+``` 
+> db.getUsers()
+``` 
+
+``` 
+> db.createUser(......)
+
+> db.getUsers()
+``` 
+
+![alt tag](https://i.imgur.com/Mx4Y4Fv.jpg)
+
+
+* Step 4
+Enable MongoDB Auth
+
+``` 
+$ sudo vim /etc/mongod.conf
+``` 
+
+In this file add the following lines:
+``` 
+security:
+    authorization: 'enabled'
+``` 
+
+Now save and exit the config file and restart mongodb server.
+``` 
+$ sudo service mongod restart
+``` 
+
+* Step 5
+Testing to connect MongoDB via Robo 3T
+
+![alt tag](https://i.imgur.com/r7aYl0x.jpg)
+
+TroubleShooting
+==============================
+* [MongoDB: db.grantRolesToUser() method](https://www.w3resource.com/mongodb/shell-methods/user-management/db-grantRolesToUser.php)
+
+``` 
+use admin
+db.grantRolesToUser(
+   "mynewuser",
+   [ "readWrite" , { role: "read", db: "orders" } ],
+   { w: "majority" , wtimeout: 4000 }
+);
+``` 
+
+* [MongoDB: db.updateUser() method](https://www.w3resource.com/mongodb/shell-methods/user-management/db-updateUser.php)
+``` 
+db.updateUser( "mynewuser",
+               {
+                 customData : { employeeId : "0x3039" },
+                 roles : [
+                           { role : "read", db : "assets"  }
+                         ]
+                }
+             );
+``` 
+
+* [MongoDB: db.dropUser() method](https://www.w3resource.com/mongodb/shell-methods/user-management/db-dropUser.php)
+``` 
+> db.dropUser("mynewuser", {w: "majority", wtimeout: 4000});
+true
+``` 
+
 
 Environment
 ==============================
@@ -20,6 +118,27 @@ Reference
 * [[Python] Pandas dataframe 資料儲存至 MongoDB](https://oranwind.org/python-pandas-ji-chu-jiao-xue-2/)
 * [Flask扩展系列(五)–MongoDB](http://www.bjhee.com/flask-ext5.html)
 * [Python利用ORM控制MongoDB（MongoEngine）的步骤全纪录](https://www.jb51.net/article/147379.htm)
+* [Python與資料分析](https://sites.google.com/site/zsgititit/shi-yongpython-jin-xing-zi-liao-fen-xi)
+* [connecting to MongoDB with authentication using pymongo 2.7](https://stackoverflow.com/questions/41769875/connecting-to-mongodb-with-authentication-using-pymongo-2-7)
+``` 
+After created a db and put some docs into collection, should also createUser() for that db, than pymongo can access.
+``` 
+
+* [Centralized User Data](https://docs.mongodb.com/v3.6/core/security-users/#centralized-user-data)
+``` 
+Changed in version 2.6.
+
+MongoDB stores all user information, including name, password, and the user's authentication database, in the system.users collection in the admin database.
+
+Do not access this collection directly but instead use the user management commands.
+``` 
+
+* [MongoDB: db.updateUser() method](https://www.w3resource.com/mongodb/shell-methods/user-management/db-updateUser.php)
+* [MongoDB: db.dropUser() method](https://www.w3resource.com/mongodb/shell-methods/user-management/db-dropUser.php)
+* [MongoDB: db.grantRolesToUser() method](https://www.w3resource.com/mongodb/shell-methods/user-management/db-grantRolesToUser.php)
+
+* [MongoError: not authorized on to execute command { find: “app_updates”, filter: { key: “0.0.1-admins” }, limit: 1, batchSize: 1, singleBatch: true }](https://stackoverflow.com/questions/47130379/mongoerror-not-authorized-on-to-execute-command-find-app-updates-filter)
+* [Mongodb enable authentication (Enable Access Control)](https://medium.com/@raj_adroit/mongodb-enable-authentication-enable-access-control-e8a75a26d332)
 
 * []()
 ![alt tag]()
---
+++ test/sequential_FromTwseToMongo.py
@@ -0,0 +1,127 @@
+#將股票交易資料放進MongoDB資料庫，就不用需要每次從證交所讀取資料
+#Refers from https://github.com/jang0820/Stock/blob/master/FromTwseToMongo.py
+######################################################
+
+import numpy as np
+import requests
+import pandas as pd
+import datetime
+import json
+import matplotlib.pyplot as pp
+import time,os,sys
+from pymongo import MongoClient
+#   http://www.twse.com.tw/exchangeReport/STOCK_DAY?date=20180817&stockNo=2330
+
+strabspath=os.path.abspath(__file__)
+strdirname=os.path.dirname(strabspath)
+str_split=os.path.split(strdirname)
+prevdirname=str_split[0]
+dirnamelib=os.path.join(prevdirname,"lib")
+dirnamelog=os.path.join(prevdirname,"log")
+# check log folder if exist or not
+if not os.path.isdir(dirnamelog):
+    os.mkdir(dirnamelog)
+
+sys.path.append(dirnamelib)
+import readConfig as readConfig
+from logger import logger
+
+def connect_mongo(arg, *args):  #連線資料庫
+    global collection
+    mongo_host=arg
+    mongo_db=args[0]
+    mongo_collection=args[1]
+    #print(arg,args[0],args[1])
+    mongo_username=args[2]
+    mongo_password=args[3]
+
+    uri_mongo = "mongodb://{}:{}@{}:27017" 
+
+    client = MongoClient(uri_mongo.format(mongo_username,mongo_password,mongo_host))
+    db = client[mongo_db]
+    collection = db[mongo_collection]
+    print(collection)
+
+
+def get_stock_history(date, stock_no, retry = 5):   #從www.twse.com.tw讀取資料
+    quotes = []
+    url = 'http://www.twse.com.tw/exchangeReport/STOCK_DAY?date=%s&stockNo=%s' % ( date, stock_no)
+    r = requests.get(url)
+    data = r.json()
+    print(data)
+
+    return transform(data['data'])  #進行資料格式轉換
+
+def transform_date(date):   #民國轉西元
+        y, m, d = date.split('/')
+        return str(int(y)+1911) + '/' + m  + '/' + d
+    
+def transform_data(data):   #將證交所獲得資料進行資料格式轉換
+    data[0] = datetime.datetime.strptime(transform_date(data[0]), '%Y/%m/%d')
+    data[1] = int(data[1].replace(',', ''))#把千進位的逗點去除
+    data[2] = int(data[2].replace(',', ''))
+    data[3] = float(data[3].replace(',', ''))
+    data[4] = float(data[4].replace(',', ''))
+    data[5] = float(data[5].replace(',', ''))
+    data[6] = float(data[6].replace(',', ''))
+    data[7] = float(0.0 if data[7].replace(',', '') == 'X0.00' else data[7].replace(',', ''))  # +/-/X表示漲/跌/不比價
+    data[8] = int(data[8].replace(',', ''))
+    return data
+
+def transform(data):   #讀取每一個元素進行資料格式轉換，再產生新的串列
+    return [transform_data(d) for d in data]
+
+def genYM(smonth, syear, emonth, eyear):  #產生從syear年smonth月到eyear年emonth月的所有年與月的tuple
+    start = 12 * syear + smonth
+    end = 12 * eyear + emonth
+    for num in range(int(start), int(end) + 1):
+        y, m = divmod(num, 12)
+        yield y, m
+
+def fetch_data(year: int, month: int, stockno):  #擷取從year-month開始到目前為止的所有交易日資料
+    count_element = 0
+
+    today = datetime.datetime.today()
+    for year, month in genYM(month, year, today.month, today.year): #產生year-month到今天的年與月份，用於查詢證交所股票資料
+        if month < 10:
+            date = str(year) + '0' + str(month) + '01'  #1到9月
+        else:
+            date = str(year) + str(month) + '01'   #10月
+        data = get_stock_history(date, stockno)
+        for item in data:  #取出每一天編號為stockno的股票資料
+            if collection.find({ "date": item[0],   #找尋該交易資料是否不存在
+                    "stockno": stockno} ).count() == 0:
+                    
+                element={'date':item[0], 'stockno':stockno, 'shares':item[1], 'amount':item[2], 'open':item[3], 'high':item[4], 
+                     'low':item[5], 'close':item[6], 'diff':item[7], 'turnover':item[8]};  #製作MongoDB的插入元素
+                print(element)
+                collection.insert_one(element)  #插入元素到MongoDB
+                count_element += 1   #caluate element numbers
+
+        time.sleep(5)  #延遲5秒，證交所會根據IP進行流量統計，流量過大會斷線
+
+    return count_element   
+
+if __name__ == '__main__':
+    t0 = time.time()
+    count = 0
+
+    configPath=os.path.join(strdirname,"config.ini")
+    localReadConfig = readConfig.ReadConfig(configPath)
+
+    mongo_host = localReadConfig.get_MongoDB('mongo_host')
+    mongo_db = localReadConfig.get_MongoDB('mongo_db')
+    mongo_collection = localReadConfig.get_MongoDB('mongo_collection')
+    mongo_username = localReadConfig.get_MongoDB('mongo_username') 
+    mongo_password = localReadConfig.get_MongoDB('mongo_password') 
+
+    str_last_year_month = localReadConfig.get_SeymourExcel("last_year_month")
+    str_stkidx =  localReadConfig.get_SeymourExcel("stkidx")
+    str_last_year = str_last_year_month.split(',')[0]
+    str_last_month = str_last_year_month.split(',')[1]
+
+    connect_mongo(mongo_host,mongo_db,mongo_collection,mongo_username,mongo_password)   #連線資料庫
+    count = fetch_data(int(str_last_year), int(str_last_month), str_stkidx)   #取出編號stkidx的股票，從str_last_year_month到今天的股價與成交量資料
+
+    msg = '{} collection(s) downloaded in {:.2f} seconds.'
+    logger.info(msg.format(count, time.time() - t0))
